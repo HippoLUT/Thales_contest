3a21fa0444b33512432c3302f5ad8af637eb57b8
AnneCharlotteVgn <annecharlotte.vgn@gmail.com>
2023-01-27 14:10:23 +0100

    feat: launch and working zephyr with STACK_CANARIES

---
+++ zephyr-docker/workspace/build/CMakeFiles/3.20.5/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-gcc")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-ar")
+set(CMAKE_ASM_COMPILER_AR "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-gcc-ar")
+set(CMAKE_RANLIB "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-ranlib")
+set(CMAKE_ASM_COMPILER_RANLIB "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-gcc-ranlib")
+set(CMAKE_LINKER "/opt/toolchains/zephyr-sdk-0.15.1/riscv64-zephyr-elf/bin/riscv64-zephyr-elf-ld.bfd")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
